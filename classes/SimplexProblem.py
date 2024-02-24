dfb6a0c003c145b355d37221300dcc0ed7196276
tahashieenavaz <tahashieenavaz@gmail.com>
2024-02-24 16:03:30 +0100

    wip

--- classes/SimplexProblem.py
+++ classes/SimplexProblem.py
@@ -4,7 +4,6 @@ from classes.ConstraintCollection import ConstraintCollection
 
 
 class SimplexProblem:
-    def __init__(self, objective_function: ObjectiveFunction, constraints: ConstraintCollection) -> SimplexAnswer:
-        self.objective_function = objective_function
+    def __init__(self, objective_function: ObjectiveFunction, constraints: ConstraintCollection):
         self.objective_function = objective_function
         self.constraints = constraints
