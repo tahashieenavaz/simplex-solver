4f91dceeb7416b77600503edc77c51a6f65ef672
tahashieenavaz <tahashieenavaz@gmail.com>
2024-02-26 16:23:52 +0100

    wip

--- src/enums/Sign.py
+++ src/enums/Sign.py
@@ -3,16 +3,21 @@ from enum import Enum
 
 class Sign(Enum):
     GREATEREQUAL = ">="
+    GreaterEqual = ">="
     GE = ">="
 
     GREATER = ">"
+    Greater = ">"
     G = ">"
 
     SMALLEREQUAL = "<="
+    SmallerEqual = "<="
     SE = "<="
 
     SMALLER = "<"
+    Smaller = "<"
     S = "<"
 
     EQUAL = "="
+    Equal = "="
     E = "="
